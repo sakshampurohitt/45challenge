5afeb0a00b605ab44d63806c588d90256f73e715
sakshampurohitt <127320787+sakshampurohitt@users.noreply.github.com>
2024-08-03 22:26:23 +0530

    Create BalancedTree.cpp

---
+++ BalancedTree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<bool, int> isBalfast(TreeNode* root){
+        if(root==NULL){
+             pair<bool, int> p;
+             p = make_pair(true, 0);
+             return p;
+        }
+        pair<bool, int> left = isBalfast(root->left);
+        pair<bool, int> right = isBalfast(root->right);
+        bool diff = abs(left.second - right.second)<=1;
+        pair<bool, int> ans;
+        ans.second = max(left.second, right.second)+1;
+        if(left.first && right.first && diff) ans.first = true;
+        else ans.first = false;
+
+        return ans;
+    }
+    bool isBalanced(TreeNode* root) {
+        return isBalfast(root).first;
+    }
+};
